2a8a37a2a814dcd74d309863ef84864635e7a5c2
roketyyang <yangxikun@users.noreply.github.com>
2018-07-04 11:30:18 +0800

    Create init-shell.sh

---
+++ init-shell.sh
@@ -0,0 +1,4 @@
+sudo yum install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+copy robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
+copy .zshrc ~/.zshrc
