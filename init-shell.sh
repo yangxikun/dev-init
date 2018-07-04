ae2d8bc7cadcf3de2e20abde30ae915a1d13d8d6
yangxikun <yangxikun@yangxikundeMacBook-Pro.local>
2018-07-04 11:36:02 +0800

    init

--- init-shell.sh
+++ init-shell.sh
@@ -1,4 +1,4 @@
-sudo yum install zsh
+sudo yum install zsh autojump
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 copy robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 copy .zshrc ~/.zshrc
